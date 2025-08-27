422b17047faab12db6289d0d5f017de878d303e0
Pablo Lerma <plermam@gmail.com>
2025-08-27 20:55:54 +0200

    chore: fix deploy to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,23 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const isPages = process.env.GH_PAGES === "true"; // set only for prod builds
+
+  return {
+    base: isPages ? "/acompanaplus/" : "/",   // root locally, subpath in prod
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+      mode === 'development' &&
+      componentTagger(),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+}});
